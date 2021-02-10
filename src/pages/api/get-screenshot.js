3a9d6617d374577c56cf25f4df3978d98945f458
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-02-10 12:02:27 -0500

    Use playwright instead

--- src/pages/api/get-screenshot.js
+++ src/pages/api/get-screenshot.js
@@ -1,7 +1,9 @@
 // import puppeteer from "puppeteer";
-import chromium from "chrome-aws-lambda";
+// import chromium from "chrome-aws-lambda";
 import sharp from "sharp";
 import browserConfig from "../../utils/browserConfig";
+// import playwright from "playwright-aws-lambda";
+const playwright = require("playwright-aws-lambda");
 
 // https://stackoverflow.com/questions/21227078/convert-base64-to-image-in-javascript-jquery
 // https://github.com/vercel/vercel/discussions/4903
@@ -22,28 +24,35 @@ export default async function handler(req, res) {
 
 	const cleanedUrl = !url.startsWith("http") ? `http://${url}` : url;
 
+	let browser = null;
+
 	const takeScreenshot = async () => {
 		try {
-			const browser = await chromium.puppeteer.launch({
-				args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-				defaultViewport: chromium.defaultViewport,
-				executablePath: await chromium.executablePath,
-				headless: true,
-				ignoreHTTPSErrors: true,
-			});
+			// const browser = await chromium.puppeteer.launch({
+			// 	args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+			// 	defaultViewport: chromium.defaultViewport,
+			// 	executablePath: await chromium.executablePath,
+			// 	headless: true,
+			// 	ignoreHTTPSErrors: true,
+			// });
 
+			// const page = await browser.newPage();
+
+			browser = await playwright.launchChromium({ headless: true });
+			// const context = await browser.newContext();
 			const page = await browser.newPage();
 
 			// Configure the navigation timeout
 			page.setDefaultNavigationTimeout(0);
 
-			await page.setViewport({
+			await page.setViewportSize({
 				width: resolution.width ?? defaultOptions.resolution.width,
 				height: resolution.height ?? defaultOptions.resolution.height,
-				deviceScaleFactor: 1,
+				// deviceScaleFactor: 1,
 			});
 
-			await page.goto(cleanedUrl, { waitUntil: "networkidle2" });
+			// await page.goto(cleanedUrl, { waitUntil: "networkidle2" });
+			await page.goto(cleanedUrl, { waitUntil: "networkidle" });
 
 			const screenshot = await page.screenshot();
 
