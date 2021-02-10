ce349aa030bca9888d4fd111d14bcaa2f4cb0830
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-02-10 07:13:21 -0500

    Use chrome-aws-lambda only

--- src/pages/api/get-screenshot.js
+++ src/pages/api/get-screenshot.js
@@ -1,52 +1,10 @@
+// import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 import sharp from "sharp";
 import browserConfig from "../../utils/browserConfig";
-import chrome from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
 
-// let chrome = {};
-// let puppeteer;
-
-// https://blog.maximeheckel.com/posts/creating-beautiful-screenshots-source-code-with-serverless-function
-const isDev = process.env.NODE_ENV === "development";
-
-const exePath =
-	process.platform === "win32"
-		? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-		: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-export const getOptions = async () => {
-	/**
-	 * If used in a dev environment, i.e. locally, use one of the local
-	 * executable path
-	 */
-	if (isDev) {
-		return {
-			args: [],
-			executablePath: exePath,
-			headless: true,
-		};
-	}
-	/**
-	 * Else, use the path of chrome-aws-lambda and its args
-	 */
-	return {
-		args: chrome.args,
-		executablePath: await chrome.executablePath,
-		headless: chrome.headless,
-	};
-};
-
-// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-// 	// running on the Vercel platform.
-// 	chrome = require("chrome-aws-lambda");
-// 	puppeteer = require("puppeteer-core");
-// } else {
-// 	puppeteer = require("puppeteer");
-// 	// running locally.
-// }
-
-// https://github.com/vercel/vercel/discussions/4903
 // https://stackoverflow.com/questions/21227078/convert-base64-to-image-in-javascript-jquery
+// https://github.com/vercel/vercel/discussions/4903
 
 export default async function handler(req, res) {
 	if (req.method !== "POST") res.status(405).end(); // Method not allowed;
@@ -66,14 +24,13 @@ export default async function handler(req, res) {
 
 	const takeScreenshot = async () => {
 		try {
-			const options = await getOptions(isDev);
-			// const browserFetcher = puppeteer.createBrowserFetcher();
-			// const revisionInfo = await browserFetcher.download("848005");
-			// const browser = await puppeteer.launch({ executablePath: revisionInfo.executablePath });
-
-			// const browser = await puppeteer.launch({ headless: true });
-
-			const browser = await puppeteer.launch({ options });
+			const browser = await chromium.puppeteer.launch({
+				args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+				defaultViewport: chromium.defaultViewport,
+				executablePath: await chromium.executablePath,
+				headless: true,
+				ignoreHTTPSErrors: true,
+			});
 
 			const page = await browser.newPage();
 
