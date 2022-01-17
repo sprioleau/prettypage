c21d4937ee1c1b9511cd083d2b0b875fa64b332c
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-17 12:54:37 -0500

    :recycle: Update Homepage component styling and logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,53 @@
-import { useState, useRef } from "react";
-import axios from "axios";
-import { useWindowSize } from "react-use";
-import Confetti from "react-confetti";
-
-import Head from "../components/Head";
-import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import screens from "../utils/screens";
-import { TwitterPicker } from "react-color";
-import { BiArrowBack } from "react-icons/bi";
-import { FiDownload } from "react-icons/fi";
-import { CgDarkMode } from "react-icons/cg";
-import saveScreenshot from "../utils/saveScreenshot";
 import {
-	InputGroup,
-	InputLeftAddon,
-	Input,
-	Stack,
+	Box,
+	Button,
+	ButtonGroup,
 	Container,
 	Flex,
 	FormControl,
-	Select,
-	Button,
 	Image,
-	Box,
-	Text,
-	ButtonGroup,
-	RadioGroup,
+	Input,
+	InputGroup,
+	InputLeftAddon,
 	Radio,
+	RadioGroup,
+	Select,
+	Stack,
+	Text,
 	useColorMode,
 } from "@chakra-ui/react";
 
+import { BiArrowBack } from "react-icons/bi";
+import { CgDarkMode } from "react-icons/cg";
+import Confetti from "react-confetti";
+import { FiDownload } from "react-icons/fi";
+import Footer from "../components/Footer";
+import Head from "../components/Head";
+import Hero from "../components/Hero";
+import { TwitterPicker } from "react-color";
+import axios from "axios";
+import { getTimeStamp } from "../utils/getTimeStamp";
+import { saveAs } from "file-saver";
+import screens from "../utils/screens";
+import { useState } from "react";
+import { useWindowSize } from "react-use";
+
 const defaults = {
+	url: "sprioleau.dev",
 	resolution: {
 		width: screens[0].width,
 		height: screens[0].height,
 		value: screens[0].value,
 	},
 	colorMode: "light",
-	color: { hex: "#7BDCB5" },
+	color: { rgb: { r: 123, g: 220, b: 181 } },
 };
+
+const getRgbColor = ({ r, g, b }) => `rgb(${r}, ${g}, ${b})`;
 
 const Home = () => {
 	const [data, setData] = useState(null);
-	const [url, setUrl] = useState("");
+	const [url, setUrl] = useState(defaults.url);
 	const [resolution, setResolution] = useState(defaults.resolution);
 	const [colorPickerOpen, setColorPickerOpen] = useState(false);
 	const [color, setColor] = useState(defaults.color);
@@ -53,14 +57,10 @@ const Home = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 	const { width, height } = useWindowSize();
 
-	const home = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://prettypage.vercel.app";
-
 	const placeholders = {
 		resolution: "Screenshot resolution",
 		colorMode: "Color mode",
 	};
-
-	// const colorPickerRef = useRef(null);
 
 	const options = { url, resolution, color: color.rgb, mode: screenshotColorMode };
 	const config = {
@@ -76,26 +76,22 @@ const Home = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		setLoading(true);
-		const response = await axios.post(`${home}/api/screenshot`, { options }, config);
-		const data = await response.data;
-		console.log("data:", data);
+		console.log("options:", options);
+		const { data } = await axios.post("/api/screenshot", { options }, config);
 		setLoading(false);
 		setData(data);
 	};
 
 	const handleSelectResolution = (e) => {
-		e.preventDefault();
-		const value = e.target.value;
-		if (!value) return setResolution(defaults.resolution);
-		const screen = screens.find((screen) => screen.value === value);
-		setResolution({ width: screen.width, height: screen.height, value: screen.value });
+		const inputValue = e.target.value;
+		if (!inputValue) return setResolution(defaults.resolution);
+		const { width, height, value } = screens.find((screen) => screen.value === inputValue);
+		setResolution({ width, height, value });
 	};
 
 	const handleSelectColor = (color) => {
 		setColor(color);
 	};
-
-	// useDismissOnOutsideClick(colorPickerRef, colorPickerOpen, () => setColorPickerOpen(false));
 
 	const toggleColorPickerVisibility = () => {
 		setColorPickerOpen(!colorPickerOpen);
@@ -105,27 +101,40 @@ const Home = () => {
 		setData({});
 	};
 
+	const handleDownload = () => {
+		if (!data) return;
+		saveAs(`data:image/png;base64,${data.base64String}`, `pretty-page-screenshot_${getTimeStamp()}.png`);
+	};
+
+	const pageBgColor = colorMode === "light" ? "gray.100" : "gray.900";
+
 	return (
-		<Flex
-			as="div"
-			className="app"
-			direction="column"
-			/* bgColor="gray.100" /* bgGradient="linear(to-br, #7928CA, #FF0080)" */
-		>
+		<Flex as="div" className="app" direction="column" bgColor={pageBgColor}>
 			<Head />
 			<Flex className="app" direction="column" justify="space-between" height="100vh">
 				<Container display="flex" justifyContent="center" my={8}>
-					<Button onClick={toggleColorMode} leftIcon={<CgDarkMode />}>
+					<Button onClick={toggleColorMode} leftIcon={<CgDarkMode />} bgColor="gray.300" textColor="gray.900">
 						Toggle {colorMode === "light" ? "Dark" : "Light"} Mode
 					</Button>
 				</Container>
 
-				<Box as="main" px={4}>
-					<Container as="section" width="100%" maxW={600} px={8} py={12} boxShadow="xl" p="6" rounded="lg">
-						<Hero />
-						{!data?.base64String ? (
+				<Box as="main" px={4} bgColor={pageBgColor}>
+					{!data?.base64String ? (
+						<Container
+							as="section"
+							width="100%"
+							maxW={600}
+							boxShadow="xl"
+							p={8}
+							borderRadius="xl"
+							mb={24}
+							bgColor={colorMode === "light" ? "white" : "gray.900"}
+							border="1px solid"
+							borderColor="gray.400"
+						>
 							<form onSubmit={handleSubmit}>
-								<Stack spacing={4}>
+								<Stack spacing={4} my={6}>
+									<Hero />
 									<FormControl id="url-input" isRequired>
 										<InputGroup size="lg">
 											<InputLeftAddon children="http://" />
@@ -149,13 +158,13 @@ const Home = () => {
 
 									<Button
 										className="color-picker"
-										bgColor={color.hex}
+										bgColor={getRgbColor(color.rgb)}
 										flexGrow="1"
 										size="lg"
 										position="relative"
 										onClick={toggleColorPickerVisibility}
-										_hover={{ backgroundColor: color.hex }}
-										_active={{ backgroundColor: color.hex }}
+										_hover={{ backgroundColor: getRgbColor(color.rgb) }}
+										_active={{ backgroundColor: getRgbColor(color.rgb) }}
 									>
 										<Text fontSize="md" color="gray.900">
 											Select Color
@@ -179,58 +188,59 @@ const Home = () => {
 									</Button>
 								</Stack>
 							</form>
-						) : (
-							<Container as="section" width="100%" maxW="unset" px={0}>
-								<Flex align="center" justify="center" direction="column">
-									<Confetti
-										width={width}
-										height={height}
-										numberOfPieces={250}
-										colors={["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4"]}
-										recycle={false}
-									/>
-									<Image
-										src={`data:image/png;base64,${data.base64String}`}
-										alt={`screenshot of ${url}`}
-										width="100%"
-										height="auto"
-										display="block"
-										mb={4}
-										className="screenshot"
-									/>
-									<ButtonGroup
-										display="flex"
-										flexWrap="wrap"
-										style={{ gap: "1rem" }}
-										width="100%"
-										justifyContent="center"
+						</Container>
+					) : (
+						<Container as="section" width="clamp(300px, 90%, 1000px)" maxW="unset" px={6}>
+							<Flex align="center" justify="center" direction="column">
+								<Confetti
+									width={width}
+									height={height}
+									numberOfPieces={250}
+									colors={["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4"]}
+									recycle={false}
+								/>
+								<Image
+									src={`data:image/png;base64,${data.base64String}`}
+									alt={`screenshot of ${url}`}
+									width="100%"
+									height="auto"
+									display="block"
+									mb={4}
+									className="screenshot"
+									borderRadius="lg"
+								/>
+								<ButtonGroup
+									display="flex"
+									flexWrap="wrap"
+									style={{ gap: "1rem" }}
+									width="100%"
+									justifyContent="center"
+								>
+									<Button
+										size="lg"
+										leftIcon={<BiArrowBack />}
+										colorScheme="purple"
+										variant="outline"
+										onClick={handleReset}
+										flex="1 1 48%"
 									>
-										<Button
-											size="lg"
-											leftIcon={<BiArrowBack />}
-											colorScheme="purple"
-											variant="outline"
-											onClick={handleReset}
-											flex="1 1 48%"
-										>
-											Return Home
-										</Button>
-										<Button
-											size="lg"
-											leftIcon={<FiDownload />}
-											colorScheme="purple"
-											variant="solid"
-											onClick={saveScreenshot}
-											style={{ marginLeft: 0 }}
-											flex="1 1 48%"
-										>
-											Download
-										</Button>
-									</ButtonGroup>
-								</Flex>
-							</Container>
-						)}
-					</Container>
+										Return Home
+									</Button>
+									<Button
+										size="lg"
+										leftIcon={<FiDownload />}
+										colorScheme="purple"
+										variant="solid"
+										onClick={handleDownload}
+										style={{ marginLeft: 0 }}
+										flex="1 1 48%"
+									>
+										Download
+									</Button>
+								</ButtonGroup>
+							</Flex>
+						</Container>
+					)}
 				</Box>
 
 				<Footer />
