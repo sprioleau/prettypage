641d2622e10d49a14206371b91bc3a3866f51096
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-17 12:53:20 -0500

    Update utils

---
+++ src/utils/getTimeStamp.js
@@ -0,0 +1,11 @@
+export const getTimeStamp = () => {
+	const [date, time] = new Date()
+		.toLocaleString()
+		.split(",")
+		.map((item) => item.trim());
+
+	const dateString = date.replaceAll("/", "-");
+	const timeString = time.replaceAll(":", ".").replaceAll(" ", "");
+
+	return `${dateString}_${timeString}`;
+};
