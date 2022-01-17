8faaff7503917c4f99b5f45afc819df4c0d518ef
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-17 13:39:07 -0500

    Add metadata

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,23 @@
-import React from "react";
 import Head from "next/head";
+import React from "react";
 
 const HTMLHead = () => (
 	<Head>
 		<title>Pretty Page</title>
-		<link rel="icon" href="/favicon.ico" />
+		<meta name="description" content="Take pixel-perfect screenshots of any website" />
+		<meta itemprop="name" content="Pretty Page" />
+		<meta itemprop="description" content="Take pixel-perfect screenshots of any website" />
+		<meta itemprop="image" content="https://prettypage.vercel.app/images/pretty-page-social-card.png" />
+		<meta property="og:url" content="https://prettypage.vercel.app" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Pretty Page" />
+		<meta property="og:description" content="Take pixel-perfect screenshots of any website" />
+		<meta property="og:image" content="https://prettypage.vercel.app/images/pretty-page-social-card.png" />
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:title" content="Pretty Page" />
+		<meta name="twitter:description" content="Take pixel-perfect screenshots of any website" />
+		<meta name="twitter:image" content="https://prettypage.vercel.app/images/pretty-page-social-card.png" />
+		<link rel="icon" href="/favicon.png" />
 	</Head>
 );
 
