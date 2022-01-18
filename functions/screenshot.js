a32d837f91cc2b1797ee2161d4e047382423b9d1
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-18 13:13:36 -0500

    Attempt at using playwright

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -1,24 +1,40 @@
-import { browserConfig, defaultOptions } from "../src/constants";
+const { chromium } = require("playwright");
+const chromiumLambda = require("chrome-aws-lambda");
+const sharp = require("sharp");
 
-import chromium from "chrome-aws-lambda";
-import { getCleanedUrl } from "../src/utils";
-import puppeteer from "puppeteer-core";
-import sharp from "sharp";
+const defaultOptions = {
+	url: "https://web.dev",
+	resolution: {
+		width: 1280,
+		height: 720,
+	},
+	colorMode: "light",
+	color: { rgb: { r: 123, g: 220, b: 181 } },
+};
 
-const handler = async function (event, context) {
+const browserConfig = {
+	paddingX: 150,
+	paddingY: 120,
+	toolbarHeight: 50,
+};
+
+const getCleanedUrl = (url) => (!url.startsWith("http") ? `http://${url}` : url);
+
+exports.handler = async function (event, context) {
 	const { url, resolution, color, mode } = JSON.parse(event.body);
 
-	const browser = await puppeteer.launch({
-		executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+	const browser = await chromium.launch({
+		// const browser = await puppeteer.launch({
+		executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromiumLambda.executablePath),
 		args: [
-			...chromium.args,
-			// "--disable-gpu",
-			// "--disable-dev-shm-usage",
-			// "--disable-setuid-sandbox",
-			// "--no-first-run",
-			// "--no-sandbox",
-			// "--no-zygote",
-			// "--single-process",
+			...chromiumLambda.args,
+			"--disable-gpu",
+			"--disable-dev-shm-usage",
+			"--disable-setuid-sandbox",
+			"--no-first-run",
+			"--no-sandbox",
+			"--no-zygote",
+			"--single-process",
 		],
 		headless: true,
 		defaultViewport: {
@@ -29,8 +45,12 @@ const handler = async function (event, context) {
 
 	const page = await browser.newPage();
 
-	await page.goto(getCleanedUrl(url), { waitUntil: "networkidle2" });
-	await page.waitForTimeout(2000);
+	await page.goto(
+		getCleanedUrl(url)
+		// { waitUntil: "networkidle" }
+	);
+	// await page.goto(getCleanedUrl(url), { waitUntil: "networkidle2" });
+	// await page.waitForTimeout(2000);
 
 	const screenshot = await page.screenshot({ type: "png" });
 
@@ -58,4 +78,4 @@ const handler = async function (event, context) {
 	};
 };
 
-export { handler };
+// export { handler };
