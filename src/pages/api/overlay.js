57caf85384a6ae7ec23a6a9de2941b50ac8e5c93
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-22 17:50:10 -0500

    Update overlay to use image buffer as input for sharp

--- src/pages/api/overlay.js
+++ src/pages/api/overlay.js
@@ -1,21 +1,24 @@
-// import axios from "axios";
+import axios from "axios";
 import sharp from "sharp";
 
 export default async function handler(req, res) {
 	const { base64String } = req.body;
-	// console.log("base64String:", base64String);
 
 	const rgb = "210,111,181";
 
+	// TODO: Update home page, determine the absolute path is necessary
 	// const home =
 	// 	process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://prettypage-sprioleau.vercel.app";
 
-	// const { data } = await axios.get(`${home}/api/screenshot`);
+	// TODO: Destructure the BASE64 string
+	const { data: overlayImage } = await axios.get("/images/assets/image.png", { responseType: "arraybuffer" });
 
+	// TODO: Investigate getting the buffer from the request to minimize response times
+	const sharpImage = Buffer.from(overlayImage, "base64");
 	const screenshot = Buffer.from(base64String, "base64");
 
 	try {
-		let image = sharp("images/assets/image.png").composite([
+		let image = sharp(sharpImage).composite([
 			{
 				input: screenshot,
 				top: 170,
@@ -28,6 +31,7 @@ export default async function handler(req, res) {
 			image.flatten({ background: { r, g, b } });
 		}
 
+		// TODO: Can I use image.toString("base64")?
 		const buffer2 = await image.toBuffer();
 		const base64String2 = buffer2.toString("base64");
 		const imageUrl = `data:image/png;base64,${base64String2}`;
