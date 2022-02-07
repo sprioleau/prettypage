733d45390f0bae5ce4fc9cc22a16281a3aa46565
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-02-06 22:01:23 -0500

    Remove troublesome example URLs

--- src/constants/exampleUrls.js
+++ src/constants/exampleUrls.js
@@ -1,13 +1,10 @@
 const exampleUrls = [
 	"https://www.google.com/",
 	"https://www.pinterest.com/",
-	"https://www.netflix.com/",
-	"https://www.behance.net/",
 	"https://dribbble.com/",
 	"https://www.sprioleau.dev/",
 	"https://nextjs.org/",
 	"https://beta.reactjs.org/",
-	"https://www.airbnb.com/",
 ];
 
 export default exampleUrls;
