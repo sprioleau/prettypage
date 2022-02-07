c8798e0777032a2819c382682e2982c912a890f7
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-02-07 06:33:50 -0500

    Update example URLs

--- src/constants/exampleUrls.js
+++ src/constants/exampleUrls.js
@@ -2,7 +2,11 @@ const exampleUrls = [
 	"https://www.google.com/",
 	"https://www.pinterest.com/",
 	"https://dribbble.com/",
+	"https://behance.net/",
+	"https://kit.svelte.dev/",
+	"https://stackoverflow.com/",
 	"https://www.sprioleau.dev/",
+	"https://www.zillow.com/",
 	"https://nextjs.org/",
 	"https://beta.reactjs.org/",
 ];
