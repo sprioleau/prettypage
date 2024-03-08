0b5819d4fde6fcc71445fb9fd68667da86ec8532
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-03-08 14:15:03 -0500

    ♻️ Use hosted chromium tar file

--- src/pages/api/screenshot.js
+++ src/pages/api/screenshot.js
@@ -22,9 +22,10 @@ export default async function handler(req, res) {
 				args: [...chrome.args, "--hide-scrollbars", "--disable-web-security", "--enable-gpu", "--no-sandbox"],
 				defaultViewport: { width, height },
 				executablePath: await chrome.executablePath(
-					`https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+					// `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+					process.env.CHROME_EXECUTABLE_PATH
 				),
-				headless: "new",
+				headless: chrome.headless,
 				ignoreHTTPSErrors: true,
 		  }
 		: {
@@ -34,6 +35,7 @@ export default async function handler(req, res) {
 				ignoreHTTPSErrors: true,
 		  };
 
+	console.log("🚀 ~ handler ~ process.env.CHROME_EXECUTABLE_PATH:", process.env.CHROME_EXECUTABLE_PATH);
 	try {
 		const browser = await puppeteer.launch(launchOptions);
 
